0f6f45761be4df13406fed21410897496ee85278
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-21 20:21:27 +0300

    Update App.tsx
    Add private and Public route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-
+import { Route, Routes } from 'react-router-dom';
+import { AuthLayout, HomeLayout } from './layout';
+import Authorization from './page/AuthNavigation/Authorization/Authorization';
+import Registration from './page/AuthNavigation/Registration/Registration';
+import { Private, Public } from './hooks/Route';
+import UserInfo from './page/UserInfo/UserInfo';
 function App() {
   return (
-   <div>
-    hi
-   </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route
+            path="authorization"
+            element={
+              <Public
+                component={<Authorization />}
+                redirectTo="/home/userInfo"
+              />
+            }
+          />
+          <Route
+            path="registration"
+            element={
+              <Public
+                component={<Registration />}
+                redirectTo="/home/userInfo"
+              />
+            }
+          />
+        </Route>
+        <Route
+          path="/home"
+          element={<Private component={<HomeLayout />} redirectTo="/" />}
+        >
+          <Route
+            path="userInfo"
+            element={<Private component={<UserInfo/>} redirectTo="/" />}
+          />
+          <Route
+            path="phonebook"
+            element={<Private component={<>phonebook</>} redirectTo="/" />}
+          />
+        </Route>
+      </Routes>
+    </div>
   );
 }
-
+// <AuthLayout />
 export default App;
