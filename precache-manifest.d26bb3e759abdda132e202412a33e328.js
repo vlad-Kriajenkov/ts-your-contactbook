50d3c14206f73c416be59c520ee5ec5ecdacd550
vlad-Kriajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-21 18:01:23 +0000

    Deploying to gh-pages from @ vlad-Kriajenkov/ts-your-contactbook@5394007cad9e457d9a6970aebfd8e84339063d52 🚀

---
+++ precache-manifest.d26bb3e759abdda132e202412a33e328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b3801af6b01045de5b8",
+    "url": "/ts-your-contactbook/static/css/main.47ec72e6.chunk.css"
+  },
+  {
+    "revision": "8b3801af6b01045de5b8",
+    "url": "/ts-your-contactbook/static/js/main.1e11f894.chunk.js"
+  },
+  {
+    "revision": "3bee060add02d0d35c48",
+    "url": "/ts-your-contactbook/static/js/runtime~main.dd796d24.js"
+  },
+  {
+    "revision": "501da080c0b8f163f18d",
+    "url": "/ts-your-contactbook/static/js/2.7db7a7c8.chunk.js"
+  },
+  {
+    "revision": "e6cc60038a6da25ccc10",
+    "url": "/ts-your-contactbook/static/js/3.1c40b467.chunk.js"
+  },
+  {
+    "revision": "60c3034d1565188ce867ab2a0e1aa6ff",
+    "url": "/ts-your-contactbook/static/media/Background.60c3034d.png"
+  },
+  {
+    "revision": "21ee275d3ef27844c5742cf04041bd68",
+    "url": "/ts-your-contactbook/index.html"
+  }
+];
