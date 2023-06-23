2a35cf7a4ca70ee55424106b4e6ad23f8a6fdc8a
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-23 19:25:54 +0300

    Fix Redux Auth
    Add Notify, Add new Functional: LogOut, FetchCurrentUser

--- src/redux/auth/auth-operation.ts
+++ src/redux/auth/auth-operation.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { INewUser, IUserPromise, IErrorPromise, IUzerLogin } from './interface';
-
+import { INewUser, IErrorPromise, IUzerLogin } from './interface';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const token = {
@@ -19,8 +19,10 @@ export const register = createAsyncThunk(
     try {
       const { data } = await axios.post('/users/signup', user);
       token.set(data.token);
+      Notify.success('Registration successful');
       return data;
     } catch (error) {
+      Notify.failure(' Something went wrong.');
       return thunkAPI.rejectWithValue(error as IErrorPromise);
     }
   }
@@ -32,6 +34,40 @@ export const logIn = createAsyncThunk(
     try {
       const { data } = await axios.post('/users/login', user);
       token.set(data.token);
+      Notify.success('Entry successful ');
+      return data;
+    } catch (error) {
+      Notify.failure(' Something went wrong.');
+      return thunkAPI.rejectWithValue(error as IErrorPromise);
+    }
+  }
+);
+
+export const LogOut = createAsyncThunk(
+  'auth/logOut',
+  async (_: any, thunkAPI: any) => {
+    try {
+      await axios.post('/users/logout');
+      token.unset();
+    } catch (error) {
+      Notify.failure('Exit not successful.');
+      return thunkAPI.rejectWithValue(error as IErrorPromise);
+    }
+  }
+);
+
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/fresh',
+  async (_, thunkAPI: any) => {
+    const state = thunkAPI.getState();
+    const persisterToken: string = state.auth.token;
+
+    if (!persisterToken) {
+      return;
+    }
+    token.set(persisterToken);
+    try {
+      const { data } = await axios.get('/users/current');
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error as IErrorPromise);
