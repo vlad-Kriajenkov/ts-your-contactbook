344f224d9df6e953592c8acb1e24a25dc5731515
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-23 19:26:21 +0300

    Update auth-slice.ts

--- src/redux/auth/auth-slice.ts
+++ src/redux/auth/auth-slice.ts
@@ -1,12 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { logIn, register } from './auth-operation';
-import { IInitialState } from './interface';
+import { fetchCurrentUser, logIn, LogOut, register } from './auth-operation';
+import { IInitialState, IUserPromise, IAction } from './interface';
 
 const initialState: IInitialState = {
   user: { name: '', email: '' },
   token: null,
   isLoading: false,
-  isLoggedIn: false,
 };
 
 export const AuthSlice = createSlice({
@@ -20,35 +19,56 @@ export const AuthSlice = createSlice({
     },
     [register.fulfilled.toString()]: (
       state: IInitialState,
-      action: PayloadAction<{ token: string; data: any }>
+      action: PayloadAction<{ token: string; user: IUserPromise }>
     ) => {
-      state.user = action.payload.data;
+      state.user = action.payload.user;
       state.token = action.payload.token;
-      state.isLoggedIn = false;
+
       state.isLoading = true;
     },
     [register.rejected.toString()]: (state: IInitialState) => {
       state.isLoading = false;
     },
-      //! LogIn
-      [logIn.pending.toString()]: (state: IInitialState) => {
-        state.isLoading = true;
-      },
-      [logIn.fulfilled.toString()]: (
-        state: IInitialState,
-        action: PayloadAction<{ token: string; data: any }>
-      ) => {
-        state.user = action.payload.data;
-        state.token = action.payload.token;
-        state.isLoggedIn = false;
-        state.isLoading = true;
-      },
-      [logIn.rejected.toString()]: (state: IInitialState) => {
-        state.isLoading = false;
-      },
-  },
+    //! LogIn
+    [logIn.pending.toString()]: (state: IInitialState) => {
+      state.isLoading = true;
+    },
+    [logIn.fulfilled.toString()]: (
+      state: IInitialState,
+      action: PayloadAction<{ token: string; user: IUserPromise }>
+    ) => {
+      console.log(action.payload.user);
+      state.user = action.payload.user;
+      state.token = action.payload.token;
 
- 
+      state.isLoading = true;
+    },
+    [logIn.rejected.toString()]: (state: IInitialState) => {
+      state.isLoading = false;
+    },
+    //! LogOut
+
+    [LogOut.fulfilled.toString()]: (state: IInitialState) => {
+      state.user = { name: '', email: '' };
+      state.token = null;
+      state.isLoading = false;
+    },
+
+    //! FetchCurrentUser
+    [fetchCurrentUser.pending.toString()]: (state: IInitialState) => {
+      state.isLoading = true;
+    },
+    [fetchCurrentUser.fulfilled.toString()]: (
+      state: IInitialState,
+      action: IAction
+    ) => {
+      state.user = action.payload;
+      state.isLoading = true;
+    },
+    [fetchCurrentUser.rejected.toString()]: (state: IInitialState) => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export default AuthSlice.reducer;
