be3b7e156152c77078da9529eef716a66897869a
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-23 19:29:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthLayout, HomeLayout } from './layout';
 import Authorization from './page/AuthNavigation/Authorization/Authorization';
 import Registration from './page/AuthNavigation/Registration/Registration';
 import { Private, Public } from './hooks/Route';
 import UserInfo from './page/UserInfo/UserInfo';
+import PhoneBook from './page/PhoneBook/PhoneBook';
+import { useAppDispatch, useAppSelector } from './redux/hook';
+import { fetchCurrentUser } from './redux/auth/auth-operation';
+import { getContacts } from './redux/contacts/contact-operation';
+
 function App() {
+  const dispatch = useAppDispatch();
+  const token  = useAppSelector(state => state.auth.token)
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getContacts());
+    }
+  }, [token, dispatch]);
+
   return (
     <div>
       <Routes>
@@ -35,11 +52,11 @@ function App() {
         >
           <Route
             path="userInfo"
-            element={<Private component={<UserInfo/>} redirectTo="/" />}
+            element={<Private component={<UserInfo />} redirectTo="/" />}
           />
           <Route
             path="phonebook"
-            element={<Private component={<>phonebook</>} redirectTo="/" />}
+            element={<Private component={<PhoneBook />} redirectTo="/" />}
           />
         </Route>
       </Routes>
