f9f09340efe7ee6215cdef35c210a00c9ab5d096
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-23 19:29:05 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReduser from './auth/auth-slice';
-
+import contactReduser from './contacts/contact-slice';
 import {
   persistStore,
   persistReducer,
@@ -11,7 +11,6 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-
 import storage from 'redux-persist/lib/storage';
 
 const authPersistConfig = {
@@ -19,10 +18,16 @@ const authPersistConfig = {
   storage,
   whitelist: ['token'],
 };
+
+const reduser = combineReducers({
+  auth: persistReducer(authPersistConfig, authReduser),
+  contacts: contactReduser,
+});
+
+export type RootReducer = ReturnType<typeof reduser>;
+
 export const store = configureStore({
-  reducer: {
-    auth: persistReducer(authPersistConfig, authReduser),
-  },
+  reducer: reduser,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -34,3 +39,5 @@ export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
